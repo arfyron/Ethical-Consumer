cfd5dab219b0c14aac64d7ad044c4f6443529cfc
Ari <ari.efron@gmail.com>
2019-02-01 16:02:03 -0500

    Added axios request to get executive information

--- Server/controllers/controller.js
+++ Server/controllers/controller.js
@@ -1,6 +1,13 @@
+const axios = require('axios');
+const APIKEYS = require('../../config.js')
 module.exports = {
   getExecs : (req, res) => {
     //query api for execs based on company name or SEC ID number
+    let {ticker} = req.params;
+    axios.get(`https://api.intrinio.com/executives?company=${ticker}&type=us&api_key=${APIKEYS.intrinoAPI}`)
+    .then(({data}) => {
+      res.send(data)
+    })
   },
 
   getDonations : (req, res) => {
