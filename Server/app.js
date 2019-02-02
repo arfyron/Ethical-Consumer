ab7329ff8f76ad958443b8561c22df8355a5a787
Ari <ari.efron@gmail.com>
2019-02-02 11:17:57 -0500

    Renders buttons with executive names allows those buttons to be clicked to run a government donation search.

--- Server/app.js
+++ Server/app.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'))
 app.use(express.json());
 app.use(compression())
 
-app.use(express.static(path.join(__dirname, "/../public")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use('/', routes);
 
