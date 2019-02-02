ca65a842fc833405c1779bde6966da32c2fbcddf
Ari <ari.efron@gmail.com>
2019-02-02 13:20:59 -0500

    Searches for donations going back to 2008

--- Server/controllers/controller.js
+++ Server/controllers/controller.js
@@ -39,16 +39,28 @@ module.exports = {
     let {
       name
     } = req.params;
-    const donationQuery = `https://api.open.fec.gov/v1/schedules/schedule_a/?two_year_transaction_period=2018&recipient_committee_type=C&recipient_committee_type=D&recipient_committee_type=E&recipient_committee_type=H&recipient_committee_type=I&recipient_committee_type=N&recipient_committee_type=O&recipient_committee_type=P&recipient_committee_type=Q&recipient_committee_type=S&recipient_committee_type=U&recipient_committee_type=V&recipient_committee_type=W&recipient_committee_type=X&recipient_committee_type=Y&recipient_committee_type=Z&sort_hide_null=true&contributor_type=individual&is_individual=true&contributor_zip=&per_page=100&api_key=${APIKEYS.dataGovAPI}&sort=contribution_receipt_date&contributor_name=${name}`
-    axios.get(donationQuery)
-      .then(({
-        data
-      }) => {
-        res.send(data.results)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    var dataArr = []
+    redisClient.get(name, (err, results) => {
+      if (results !== null) {
+        res.send(JSON.parse(results))
+      } else {
+        const years = [2018, 2016, 2014, 2012, 2010, 2008]
+        const promiseCalls = years.map(year => {
+          const donationQuery = `https://api.open.fec.gov/v1/schedules/schedule_a/?two_year_transaction_period=${year}&recipient_committee_type=C&recipient_committee_type=D&recipient_committee_type=E&recipient_committee_type=H&recipient_committee_type=I&recipient_committee_type=N&recipient_committee_type=O&recipient_committee_type=P&recipient_committee_type=Q&recipient_committee_type=S&recipient_committee_type=U&recipient_committee_type=V&recipient_committee_type=W&recipient_committee_type=X&recipient_committee_type=Y&recipient_committee_type=Z&sort_hide_null=true&contributor_type=individual&is_individual=true&contributor_zip=&per_page=100&api_key=${APIKEYS.dataGovAPI}&sort=contribution_receipt_date&contributor_name=${name}`
+          return axios.get(donationQuery)
+        })
+        Promise.all(promiseCalls)
+          .then((promiseResults) => {
+            promiseResults.map((individualCall) => {
+              dataArr = dataArr.concat(individualCall.data.results)
+            })
+            res.send(dataArr)
+          })
+
+
+      }
+    })
+
 
   }
 }
